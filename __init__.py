d3010b0439925edc1c3097a76aba1588cc7162e4
Zhongmin Zhu <zz4@illinois.edu>
2022-03-22 14:20:12 -0500

    solved subdir conflicrt -7

--- __init__.py
+++ __init__.py
@@ -1,3 +1,1 @@
-from bsl_inst_control.bsl_inst import bsl_instrument
-from bsl_inst_control.bsl_lib import _bsl_inst_info, _bsl_serial, _bsl_visa
-from bsl_inst_control.bsl_lib.Instruments import *
+from bsl_inst import bsl_inst
